d4acb1259d21813450362453917d1d2f1f6260b3
Robert M. Ryan <robert.ryan@mindspring.com>
2015-12-28 13:15:57 -0800

    Add MacOS target

---
+++ SampleFrameworkMacOS/SampleFrameworkMacOS.h
@@ -0,0 +1,19 @@
+//
+//  SampleFrameworkMacOS.h
+//  SampleFrameworkMacOS
+//
+//  Created by Robert Ryan on 12/28/15.
+//  Copyright © 2015 Robert Ryan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SampleFrameworkMacOS.
+FOUNDATION_EXPORT double SampleFrameworkMacOSVersionNumber;
+
+//! Project version string for SampleFrameworkMacOS.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkMacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFrameworkMacOS/PublicHeader.h>
+
+
