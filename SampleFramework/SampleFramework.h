299d586faca9a3a1ec1ec587df9510823ae9985e
Robert M. Ryan <robert.ryan@mindspring.com>
2015-12-28 13:07:15 -0800

    Initial Commit

---
+++ SampleFramework/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by Robert Ryan on 12/28/15.
+//  Copyright © 2015 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
